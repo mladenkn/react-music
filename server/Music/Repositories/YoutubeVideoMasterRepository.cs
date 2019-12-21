ecdc252d6f62b286d92d0add45109362f26660b2
Mladen <mladen.knezovic@pseudocode.agency>
2019-12-21 20:51:12 +0100

    using YoutubeHtmlScrapperVideoRepository in YoutubeVideoMasterRepository on search

--- server/Music/Repositories/YoutubeVideoMasterRepository.cs
+++ server/Music/Repositories/YoutubeVideoMasterRepository.cs
@@ -11,28 +11,44 @@ namespace Music.Repositories
     {
         private readonly YoutubeDataApiVideoRepository _remoteRepo;
         private readonly YoutubeVideoMongoRepository _localRepo;
+        private readonly YoutubeHtmlScrapperVideoRepository _youtubeHtmlScrapperVideoRepository;
 
         public YoutubeVideoMasterRepository(YoutubeDataApiVideoRepository remoteRepo,
-            YoutubeVideoMongoRepository localRepo)
+            YoutubeVideoMongoRepository localRepo, YoutubeHtmlScrapperVideoRepository youtubeHtmlScrapperVideoRepository)
         {
             _remoteRepo = remoteRepo;
             _localRepo = localRepo;
+            _youtubeHtmlScrapperVideoRepository = youtubeHtmlScrapperVideoRepository;
         }
 
-        public async Task<IEnumerable<YoutubeVideo>> GetList(IReadOnlyCollection<string> ids)
+        public async Task<IEnumerable<YoutubeVideo>> GetList(
+            IReadOnlyCollection<string> ids, Func<IEnumerable<string>, 
+            IEnumerable<string>> prepareNotFoundIdsForSearch = null
+            )
         {
             var (videosSavedLocally, notFoundVideosIds) = await _localRepo.GetList(ids);
-            if (notFoundVideosIds.Count() == 0)
+            if (notFoundVideosIds.Count == 0)
                 return videosSavedLocally;
             else
             {
-                var remotelyFetchedVideos = await _remoteRepo.GetList(notFoundVideosIds);
+                var notFoundIdsForFetch = prepareNotFoundIdsForSearch == null ? 
+                    notFoundVideosIds : 
+                    prepareNotFoundIdsForSearch(notFoundVideosIds).ToArray();
+                var remotelyFetchedVideos = await _remoteRepo.GetList(notFoundIdsForFetch);
                 await _localRepo.Save(remotelyFetchedVideos);
                 return videosSavedLocally.Concat(remotelyFetchedVideos);
             }
         }
 
-        public Task<IReadOnlyCollection<YoutubeVideoFromSearchResults>> Search(YoutubeTrackQuery query) => _remoteRepo.Search(query);
+        public async Task<IReadOnlyCollection<YoutubeVideo>> Search(YoutubeTrackQuery query)
+        {
+            var vidIds = await _youtubeHtmlScrapperVideoRepository.SearchIds(query.SearchQuery);
+            var videos = await GetList(
+                vidIds.ToArray(), 
+                notFoundIds => notFoundIds.Take(50)
+            );
+            return videos.ToArray();
+        }
     }
     
     public class YoutubeTrackQuery
