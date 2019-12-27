b1d48565c11739f4a3aa872195941264b4cd7729
Mladen <mladen.knezovic@pseudocode.agency>
2019-12-27 17:27:17 +0100

    added ServerTest - base class for tests

--- server/Operations/FeatureTests/QueryTracksViaYoutube.cs
+++ server/Operations/FeatureTests/QueryTracksViaYoutube.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Google.Apis.YouTube.v3.Data;
@@ -28,8 +29,6 @@ namespace Executables.FeatureTests
         [Fact]
         public async Task Run()
         {
-            var builder = new WebHostBuilder().UseStartup<Startup>();
-
             var searchedVideoIds = new[]
             {
                 _gen.String(), _gen.String(), _gen.String(),
@@ -54,7 +53,7 @@ namespace Executables.FeatureTests
                 }),
             };
 
-            var videosFromApiList = new []
+            var videosFromApiList = new[]
             {
                 _gen.YoutubeVideoModel(v =>
                 {
@@ -70,47 +69,117 @@ namespace Executables.FeatureTests
 
             var shouldBeVideoIdsInDbAtTheEnd = Enumerable.Union(searchedVideoIds, videosInDb.Select(v => v.Id));
 
+            await ServerTest.New()
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<SearchYoutubeVideosIds>(_ => async searchQuery => searchedVideoIds);
+                    services.AddTransient<ListYoutubeVideos>(_ => async (parts, ids) =>
+                    {
+                        Assert.True(CollectionUtils.AreEquivalentNoOrder(ids, videosFromApiList.Select(v => v.Id)));
+                        return videosFromApiList;
+                    });
+                })
+                .PrepareDatabase(db =>
+                {
+                    db.AddRange(videosInDb);
+                    db.SaveChanges();
+                })
+                .Act(httpClient => httpClient.GetAsync("api/tracks/yt?searchQuery=mia"))
+                .Assert(async (serverResponse, db) =>
+                {
+                    Assert.Equal(HttpStatusCode.OK, serverResponse.StatusCode);
+
+                    var responseContent =
+                        JsonConvert.DeserializeObject<TrackModel[]>(await serverResponse.Content.ReadAsStringAsync());
+
+                    Assert.True(
+                        CollectionUtils.AreEquivalentNoOrder(searchedVideoIds,
+                            responseContent.Select(t => t.YoutubeVideoId))
+                    );
+
+                    var allVideoIds = db.YoutubeVideos.Select(v => v.Id);
+                    Assert.True(CollectionUtils.AreEquivalentNoOrder(shouldBeVideoIdsInDbAtTheEnd, allVideoIds));
+                })
+                .Run();
+        }
+    }
+
+    public class ServerTest
+    {
+        private Action<IServiceCollection> _configureServicesActual;
+        private Action<MusicDbContext> _prepareDatabaseActual;
+        private Func<HttpClient, Task<HttpResponseMessage>> _actActual;
+        private Func<HttpResponseMessage, MusicDbContext, Task> _assertActual;
+
+        public ServerTest ConfigureServices(Action<IServiceCollection> configureServicesActual)
+        {
+            _configureServicesActual = configureServicesActual;
+            return this;
+        }
+
+        public ServerTest PrepareDatabase(Action<MusicDbContext> prepareDatabaseActual)
+        {
+            _prepareDatabaseActual = prepareDatabaseActual;
+            return this;
+        }
+
+        public ServerTest Act(Func<HttpClient, Task<HttpResponseMessage>> actActual)
+        {
+            _actActual = actActual;
+            return this;
+        }
+
+        public ServerTest Assert(Func<HttpResponseMessage, MusicDbContext, Task> assertActual)
+        {
+            _assertActual = assertActual;
+            return this;
+        }
+        
+        public async Task Run()
+        {
+            var (_, client, _, db, dispose) = CreateTestServer(_configureServicesActual);
+
+            _prepareDatabaseActual(db);
+            var serverResponse = await _actActual(client);
+            await _assertActual(serverResponse, db);
+
+            dispose();
+        }
+
+        private static (TestServer server, HttpClient client, IServiceProvider serviceProvider, MusicDbContext db, Action dispose)
+            CreateTestServer(Action<IServiceCollection> consumeServiceCollection)
+        {
+            var builder = new WebHostBuilder().UseStartup<Startup>();
             builder.ConfigureServices(services =>
             {
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType == typeof(DbContextOptions<MusicDbContext>));
-                if (descriptor != null) 
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MusicDbContext>));
+                if (descriptor != null)
                     services.Remove(descriptor);
-
                 services.AddDbContext<MusicDbContext>(o => o.UseSqlServer(Config.TestDatabaseConnectionString));
-                services.AddTransient<SearchYoutubeVideosIds>(_ => async searchQuery => searchedVideoIds);
-                services.AddTransient<ListYoutubeVideos>(_ => async (parts, ids) =>
-                {
-                    Assert.True(CollectionUtils.AreEquivalentNoOrder(ids, videosFromApiList.Select(v => v.Id)));
-                    return videosFromApiList;
-                });
+                consumeServiceCollection(services);
             });
 
-            using var server = new TestServer(builder);
+            var server = new TestServer(builder);
             var client = server.CreateClient();
             var servicesScope = server.Services.CreateScope();
-            var db = servicesScope.ServiceProvider.GetService<MusicDbContext>();
+            var serviceProvider = servicesScope.ServiceProvider;
 
+            var db = serviceProvider.GetService<MusicDbContext>();
             db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
-            db.AddRange(videosInDb);
-            db.SaveChanges();
 
-            var serverResponse = await client.GetAsync("api/tracks/yt?searchQuery=mia");
-            Assert.Equal(HttpStatusCode.OK, serverResponse.StatusCode);
-
-            var serverResponseJson = await serverResponse.Content.ReadAsStringAsync();
-            var serverResponseTracks = JsonConvert.DeserializeObject<TrackModel[]>(serverResponseJson);
-            Assert.True(
-                CollectionUtils.AreEquivalentNoOrder(searchedVideoIds, serverResponseTracks.Select(t => t.YoutubeVideoId))
-            );
-
-            using (var db_ = Helpers.UseDbContext())
+            void Dispose()
             {
-                var allVideoIds = db_.YoutubeVideos.Select(v => v.Id);
-                Assert.True(CollectionUtils.AreEquivalentNoOrder(shouldBeVideoIdsInDbAtTheEnd, allVideoIds));
-                db_.Database.EnsureDeleted();
+                db.Database.EnsureDeleted();
+                db.Dispose();
+                server.Dispose();
+                client.Dispose();
+                servicesScope.Dispose();
             }
+
+            return (server, client, serviceProvider, db, Dispose);
         }
+
+        public static ServerTest New() => new ServerTest();
     }
 }
