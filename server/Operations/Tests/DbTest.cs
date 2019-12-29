c88efd70f4c3d04e32c8a428f9dc911d0eb9b8f1
Mladen <mladen.knezovic@pseudocode.agency>
2019-12-29 19:35:30 +0100

    using ReferentialIntegrityDbTest

--- server/Operations/Tests/DbTest.cs
+++ server/Operations/Tests/DbTest.cs
@@ -35,65 +35,7 @@ namespace Executables.Tests
                 db.Database.EnsureDeleted();
             }
         }
-
-        [Fact]
-        public void Should_Fail_Trying_To_Add_Models_Because_Of_Foreign_key_constraint_violations()
-        {
-            var models = new (object model, string errorMessageContains)[]
-            {
-                (
-                    model: _gen.TrackTag(t => { t.TrackId = _gen.Int(); }), 
-                    errorMessageContains: "FK_TrackTag_Tracks_TrackId"
-                ),
-                (
-                    model: _gen.Track(t =>
-                    {
-                        t.YoutubeVideoId = "1";
-                        t.User = _gen.User();
-                    }),
-                    errorMessageContains: "FK_Tracks_YoutubeVideos_YoutubeVideoId"
-                ),
-                (
-                    model: _gen.Track(t =>
-                    {
-                        t.UserId = 1;
-                        t.YoutubeVideo = _gen.YoutubeVideo(v =>
-                        {
-                            v.Id = _gen.String();
-                            v.YoutubeChannel = _gen.YoutubeChannel(c => { c.Id = _gen.String(); });
-                        });
-                    }),
-                    errorMessageContains: "FK_Tracks_User_UserId"
-                ),
-            };
-            
-            using (var db = Utils.UseDbContext())
-            {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                foreach (var model in models)
-                {
-                    db.Add(model.model);
-                    FailAddingOne(model.model, model.errorMessageContains);
-                }
-
-                void FailAddingOne(object model, string errorMsgContains)
-                {
-                    db.Add(model);
-                    db.Invoking(_ => _.SaveChanges())
-                        .Should()
-                        .Throw<DbUpdateException>()
-                        .Where(e => e.InnerException.Message.Contains(errorMsgContains));
-                    var entries = db.ChangeTracker.Entries();
-                    foreach (var entry in entries)
-                        entry.State = EntityState.Detached;
-                }
-
-                db.Database.EnsureDeleted();
-            }
-        }
-
+        
         [Fact]
         public void Can_Add_YoutubeVideo()
         {
