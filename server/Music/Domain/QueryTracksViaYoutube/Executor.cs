0701e27d32638b95e5f0e4982f83183128fccbfd
Mladen <mladen.knezovic@pseudocode.agency>
2020-01-05 23:39:11 +0100

    tried to do end to end test

--- server/Music/Domain/QueryTracksViaYoutube/Executor.cs
+++ server/Music/Domain/QueryTracksViaYoutube/Executor.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper.QueryableExtensions;
 using Google.Apis.YouTube.v3.Data;
 using Kernel;
 using Microsoft.EntityFrameworkCore;
