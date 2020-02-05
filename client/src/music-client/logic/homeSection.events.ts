966ef83a0e56efdc81cdbeb6ac1c36f6b8eb9f74
MladenSt <mladen.knezovic.1993@gmail.com>
2020-02-05 23:48:35 +0100

    minor

--- client/src/music-client/logic/homeSection.events.ts
+++ client/src/music-client/logic/homeSection.events.ts
@@ -5,12 +5,24 @@ import { Track } from "../shared";
 
 export const updatedQueryTrackForm = createEvent('updatedQueryTrackForm', (form: TrackQueryForm) => form)
 
-export const initiatedTracksFetch = createEvent('initiatedNextPageTracksFetchFromMusicDb', (form: TrackQueryForm) => form)
+export const initiatedTracksFetch = createEvent(
+    'initiatedNextPageTracksFetchFromMusicDb', 
+    (form: TrackQueryForm) => form
+)
 export const fetchTracksFailed = createEvent('fetchTracksFailed', () => ({}))
 
-export const fetchedTracksFromMusicDb = createEvent('fetchedTracksFromMusicDb', (tracks: ArrayWithTotalCount<Track>) => tracks)
+export const fetchedTracksFromMusicDb = createEvent(
+    'fetchedTracksFromMusicDb', 
+    (tracks: ArrayWithTotalCount<Track>) => tracks
+)
 export const fetchedTracksFromYouTube = createEvent('fetchedTracksFromYoutube', (tracks: Track[]) => tracks)
 
-export const initiatedTracksNextPageFetch = createEvent('initiatedTracksNextPageFetch', (form: MusicDbTrackQueryForm) => form)
-export const fetchedTracksNextPage = createEvent('fetchedTracksNextPage', (tracks: ArrayWithTotalCount<Track>) => tracks)
+export const initiatedTracksNextPageFetch = createEvent(
+    'initiatedTracksNextPageFetch', 
+    (form: MusicDbTrackQueryForm) => form
+)
+export const fetchedTracksNextPage = createEvent(
+    'fetchedTracksNextPage', 
+    (tracks: ArrayWithTotalCount<Track>) => tracks
+)
 export const fetchTracksNextPageFailed = createEvent('fetchTracksNextPageFailed', () => ({}))
