e5dfa3abe6ec5fde9cc7452dab05d5a0669c012e
MladenSt <mladen.knezovic.1993@gmail.com>
2020-02-05 23:30:28 +0100

    updates

---
+++ client/src/utils/es/history.ts
@@ -0,0 +1,23 @@
+import { Event, EventCreator } from './events';
+import { useState } from 'react';
+
+export interface History {
+    save(event: Event): void
+    whereType<TEventArgs extends any[], TEventPayload>(eventCreators: EventCreator<TEventArgs, TEventPayload>): Event<TEventPayload>[]
+    whereTypes(eventCreators: EventCreator[]): Event[]
+}
+
+interface EventHistoryState {
+    [key: string]: Event[] | undefined
+}
+
+export const useHistory = (): History => {
+    const [state, updateState] = useState<EventHistoryState>({})
+
+    const save = (event: Event) => {
+        const eventsOfType = [event, ...state[event.type]!]
+        updateState({ ...state, [event.type]: eventsOfType })
+    }
+
+    throw new Error('Not implemented.')
+}
