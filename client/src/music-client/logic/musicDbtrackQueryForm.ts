b7e90db0aa9c8b2ea2b55a5a4a687f0df8351a3a
MladenSt <mladen.knezovic.1993@gmail.com>
2020-02-09 13:27:01 +0100

    belongs to prev commit

--- client/src/music-client/logic/musicDbtrackQueryForm.ts
+++ client/src/music-client/logic/musicDbtrackQueryForm.ts
@@ -5,46 +5,46 @@ import { useEffect } from "react";
 type Field = keyof MusicDbTrackQueryForm
 
 interface MusicDbTrackQueryFormLogic {
-    isFieldActive(fieldName: Field): boolean
-    setFieldActive(fieldName: Field): void
-    setFieldInactive(fieldName: Field): void
-    values: MusicDbTrackQueryForm
-    onFieldChange(fieldName: Field): (value: any) => void
+	isFieldActive(fieldName: Field): boolean
+	setFieldActive(fieldName: Field): void
+	setFieldInactive(fieldName: Field): void
+	values: MusicDbTrackQueryForm
+	onFieldChange(fieldName: Field): (value: any) => void
 }
 
 interface MusicDbTrackQueryFormLogicProps {
-    values: MusicDbTrackQueryForm
-    onChange: (values: MusicDbTrackQueryForm) => void
+	values: MusicDbTrackQueryForm
+	onChange: (values: MusicDbTrackQueryForm) => void
 }
 
 export const useMusicDbTrackQueryFormLogic = (props: MusicDbTrackQueryFormLogicProps): MusicDbTrackQueryFormLogic => {
-    const form = useFormik({
-        enableReinitialize: true,
-        initialValues: props.values,
-        onSubmit: () => { }
-    })
+	const form = useFormik({
+		enableReinitialize: true,
+		initialValues: props.values,
+		onSubmit: () => { }
+	})
 
-    useEffect(() => {
-        props.onChange(form.values)
-    }, [form.values])
+	useEffect(() => {
+		props.onChange(form.values)
+	}, [form.values])
 
-    const isFieldActive = (fieldName: Field) => {
-        return true
-    }
+	const isFieldActive = (fieldName: Field) => {
+		return true
+	}
 
-    const onFieldChange = (fieldName: Field) => {
-        return (value: any) => {
-            form.setFieldValue(fieldName, value)
-        }
-    }
+	const onFieldChange = (fieldName: Field) => {
+		return (value: any) => {
+			form.setFieldValue(fieldName, value)
+		}
+	}
 
-    const setFieldInactive = (fieldName: Field) => {
+	const setFieldInactive = (fieldName: Field) => {
 
-    }
+	}
 
-    const setFieldActive = (fieldName: Field) => {
+	const setFieldActive = (fieldName: Field) => {
 
-    }
+	}
 
-    return { values: form.values, isFieldActive, onFieldChange, setFieldInactive, setFieldActive }
+	return { values: form.values, isFieldActive, onFieldChange, setFieldInactive, setFieldActive }
 }
