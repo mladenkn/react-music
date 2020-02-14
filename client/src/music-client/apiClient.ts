5e8d0eac844d301a1eb32fba4ab9200451c90cbe
MladenSt <mladen.knezovic.1993@gmail.com>
2020-02-14 21:03:50 +0100

    TracksController change

--- client/src/music-client/apiClient.ts
+++ client/src/music-client/apiClient.ts
@@ -11,15 +11,15 @@ else if (process.env.NODE_ENV === "production")
 else throw new Error();
 
 export const fetchFromYouTube = async (searchQuery: string) => {
-  const r = await axios.get<Track[]>(`${baseUrl}tracks/yt`, {
-    params: { searchQuery }
+  const r = await axios.get<Track[]>(`${baseUrl}tracks`, {
+    params: { youTubeSearchQuery: searchQuery }
   });
   return r;
 };
 
 export const fetchFromMusicDb = async (query: MusicDbTrackQueryForm & Paging) => {
   const r = await axios.get<ArrayWithTotalCount<Track>>(
-    `${baseUrl}tracks`, { params: query }
+    `${baseUrl}tracks`, { params: {dbSourceParams: query}  }
   );
   return r;
 };
