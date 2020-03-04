46fb14b01b585d73ca11d689a3b06df68c599b5f
Mladen <mladen.knezovic.1993@gmail.com>
2020-03-04 17:34:24 +0100

    moved server/Music.DevOps/* to server/Music/Admin

---
+++ server/Music/Admin/Models/YouTubeChannel.cs
@@ -0,0 +1,11 @@
+﻿namespace Music.Admin.Models
+{
+    public class YouTubeChannelDetails
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int VideosCount { get; set; }
+    }
+}
---
+++ server/Music/Admin/Tasks/PersistAllChannelsVideosToFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Music.App;
+using Music.App.Services;
+
+namespace Music.Admin.Tasks
+{
+    public class PersistAllChannelsVideosToFile : ServiceResolverAware
+    {
+        public PersistAllChannelsVideosToFile(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public async Task Execute()
+        {
+            var allChannels = await Db.YouTubeChannels.Skip(4).Take(1).ToArrayAsync();
+            var ytService = Resolve<YouTubeServices>();
+            var store = Resolve<ChannelVideosPersistantStore>();
+            foreach (var youTubeChannel in allChannels)
+            {
+                try
+                {
+                    var channelWithVideos = await ytService.GetVideosOfChannel(youTubeChannel);
+                    await store.Store(channelWithVideos);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+    }
+}
