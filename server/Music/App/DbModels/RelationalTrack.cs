3f4e5be82fb60304ef8b83a339ebbc7256e665df
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-03-07 13:47:09 +0100

    removed devops project

---
+++ server/Music/App/DbModels/RelationalTrack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Kernel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music.App.DbModels
+{
+    public class RelationalTrack : IDatabaseEntity
+    {
+        public long Id { get; set; }
+
+        public IReadOnlyCollection<TrackUserProps> TrackUserProps { get; set; }
+
+        public IReadOnlyCollection<YoutubeVideo> YoutubeVideos { get; set; }
+
+        public bool IsDuplicate { get; set; }
+
+        public static void Configure(ModelBuilder modelBuilder) =>
+            modelBuilder.Entity<RelationalTrack>(c =>
+            {
+                c.ToTable("Tracks");
+            });
+    }
+}
