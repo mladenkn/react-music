b720ece7b64b9f2bb821068d4057f229d1578c0b
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-05-24 13:44:49 +0200

    minor

--- client/src/music-client/logic/musicDbtrackQueryForm.ts
+++ client/src/music-client/logic/musicDbtrackQueryForm.ts
@@ -36,7 +36,7 @@ const getInitialActiveFields = (params: MusicDbTrackQueryParams) => {
 		result.push('mustHaveEveryTag')	
 	if(params.titleContains !== '')
 		result.push('titleContains')
-	if(params.yearRange && Object.values(params.yearRange).filter(v => v).length > 0)
+	if(params.yearRange?.lowerBound || params.yearRange?.upperBound)
 		result.push('yearRange')
 	if(params.supportedYouTubeChannelsIds?.length > 0)
 		result.push('supportedYouTubeChannelsIds')
