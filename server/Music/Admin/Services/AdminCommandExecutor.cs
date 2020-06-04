3da959642f0a809a369cf9515fa5bf3da289d529
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-06-04 17:36:50 +0200

    minor

--- server/Music/Admin/Services/AdminCommandExecutor.cs
+++ server/Music/Admin/Services/AdminCommandExecutor.cs
@@ -26,29 +26,29 @@ namespace Music.Admin.Services
                     switch (type)
                     {
                         case "GetChannelDetails":
-                            {
-                                var channelId = cmd.Get<string>("channelId");
-                                var response = await ytService.GetChannelDetails(channelId);
-                                return yamlService.Serialize(response);
-                            }
+                        {
+                            var channelId = cmd.Get<string>("channelId");
+                            var response = await ytService.GetChannelDetails(channelId);
+                            return yamlService.Serialize(response);
+                        }
                         case "GetChannelsOfUser":
-                            {
-                                var username = cmd.Get<string>("username");
-                                var response = await ytService.GetChannelsOfUser(username);
-                                return yamlService.Serialize(response);
-                            }
+                        {
+                            var username = cmd.Get<string>("username");
+                            var response = await ytService.GetChannelsOfUser(username);
+                            return yamlService.Serialize(response);
+                        }
                         case "SaveChannelWithVideosToTempStorage":
-                            {
-                                var channelId = cmd.Get<string>("channelId");
-                                await Resolve<ChannelsWithVideosTempStorage>().ToTemp(channelId);
-                                return "Channel videos saved.";
-                            }
+                        {
+                            var channelId = cmd.Get<string>("channelId");
+                            await Resolve<ChannelsWithVideosTempStorage>().ToTemp(channelId);
+                            return "Channel videos saved.";
+                        }
                         case "SaveChannelWithVideosFromTempToDb":
-                            {
-                                var fileName = cmd.Get<string>("file");
-                                await Resolve<ChannelsWithVideosTempStorage>().FromTempToDb(fileName);
-                                return "Channel videos saved.";
-                            }
+                        {
+                            var fileName = cmd.Get<string>("file");
+                            await Resolve<ChannelsWithVideosTempStorage>().FromTempToDb(fileName);
+                            return "Channel videos saved.";
+                        }
                         default:
                             return "Unsupported command";
                     }
